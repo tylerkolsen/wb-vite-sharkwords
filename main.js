03e554524a036aca87180ff8900b151a8fb24714
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-12 13:41:11 -0600

    fnished through setupWord func

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import getRandomWord from './src/randomWord';
+import setSharkImage from './src/sharkImage';
+import {setupWord, isLetterInWord, revealLetterInWord} from './src/word'
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
@@ -12,7 +15,9 @@ document.querySelector('#app').innerHTML = `
 
 const initSharkwords = () => {
   let numWrong = 0;
-  const word = 'word';
+  const word = getRandomWord();
+  setSharkImage(document.querySelector('#shark-img'), numWrong)
+  setupWord(document.querySelector('#word-container'), word)
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
--- src/word.js
+++ src/word.js
@@ -1,7 +1,18 @@
+import getRandomWord from "./randomWord";
 let word;
 
-function setupWord(element, initWord) {}
+function setupWord(element, initWord) {
+    word = initWord
+    
+    word.split('').forEach(() => {
+        element.insertAdjacentHTML('beforeend', '<div class="letter-box"></div>')
+    })
+    
+     
+}
 
 function isLetterInWord(letter) {}
 
 function revealLetterInWord(letter) {}
+
+export {setupWord, isLetterInWord, revealLetterInWord}
